617033a5f2cc0b597dd9ccd777dec6b8145cf26e
RafaGallardoA <rafael.gallardo@thoughtworks.com>
2022-04-08 17:47:55 -0500

    new feature

--- src/exams/exams.test.js
+++ src/exams/exams.test.js
@@ -1,22 +1,41 @@
 const { sumGrades, promGrades, approvedExam } = require("./exams")
+let grades = []
+
+beforeAll(() => {
+    grades = [8, 7, 9, 6, 10]
+})
+
+afterAll(() => {
+   
+})
 
 describe("Exams", () => {
-    it("should sum grades", () => {
-        const grades = [8, 7, 9, 6, 10]
+
+    afterEach(() => {
+        // grades = [8, 7, 9, 6, 10]
+    });
+
+    afterEach(() => {
+
+    })
+
+    test('should be false', () => {
+        expect(false).toBeFalsy();
+    });
+
+    it("should sum grades", () => {        
         const expectedValue = String(40)
         let sum = sumGrades(grades)
         expect(sum).toEqual(expectedValue)
     });    
 
-    it("should prom grades", () => {
-        const grades = [8, 7, 9, 6, 10]
+    it("should prom grades", () => {        
         const expectedValue = String(8)
         let prom = promGrades(grades)
         expect(prom).toEqual(expectedValue)
     });
 
-    it("should know if pass exan", () => {
-        const grades = [8, 7, 9, 6, 10]
+    it("should know if pass exan", () => {        
         const expectedValue = String('Approved')
         let approved = approvedExam(grades)
         expect(approved).toEqual(expectedValue)
